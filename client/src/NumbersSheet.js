bc4b173d498088dc02829ed518614eb9811cadf8
juani15151 <15637690+juani15151@users.noreply.github.com>
2024-01-04 14:33:28 -0300

    NumbersSheet now allow to change the whole row with a single click.

--- client/src/NumbersSheet.js
+++ client/src/NumbersSheet.js
@@ -1,24 +1,18 @@
 import React from "react";
 import './NumberSheet.css';
 
+/**
+ * Independent component that lets user mark numbers to aid him in the guessing logic.
+ * e.g. When player thinks a digit is not present, they can put "X" on the entire row for that digit.
+ *
+ * A table that has all possible digits and positions, then lets the player cycle through "marks" for each combination.
+ */
 export default class NumbersSheet extends React.Component {
 
     tileRows(rowCount) {
-        let rows = [];
-
-        for (let i = 0; i < rowCount; i++) {
-            rows.push(
-                <tr key={"row-" + i}>
-                    <th>{i}</th>
-                    <Tile/>
-                    <Tile/>
-                    <Tile/>
-                    <Tile/>
-                </tr>
-            );
-        }
-
-        return rows;
+        return Array.from({length: rowCount}, (_, index) => {
+           return (<TileRow header={index}/>);
+        });
     }
 
     render() {
@@ -35,37 +29,58 @@ export default class NumbersSheet extends React.Component {
 
 }
 
-class Tile extends React.Component {
+class TileRow extends React.Component {
 
     static CHARS = [null, "O", "X", "."]
-
     constructor(props) {
         super(props);
 
+        const lastCharId = TileRow.CHARS.length - 1;
+        this.rowSize = props.rowSize || 4;
+
         this.state = {
-            letterId: Tile.CHARS.length - 1
+            valuesByTile: Array(this.rowSize).fill(lastCharId)
         };
 
+        this.header = props.header;
         this.nextChar = this.nextChar.bind(this);
     }
 
-    nextChar() {
+    nextChar(tilePosition) {
+        let valuesByTile;
+        if (typeof tilePosition !== "number") {
+            const firstValueId = this.state.valuesByTile[0];
+            console.log("First value is " + firstValueId);
+            valuesByTile = Array(this.rowSize).fill(firstValueId - 1 || TileRow.CHARS.length - 1);
+        } else {
+            valuesByTile = this.state.valuesByTile.map((letterId, position) => {
+                return position === tilePosition
+                    ? letterId - 1 || TileRow.CHARS.length - 1 // Reverse iteration that goes back to end when reaching zero.
+                    : letterId;
+            });
+        }
+
         this.setState({
-            // Will loop back to end when reaches 0.
-            letterId: this.state.letterId - 1 || Tile.CHARS.length - 1
+            valuesByTile: valuesByTile
         });
     }
-
     render() {
-        let currentLetter = Tile.CHARS[this.state.letterId];
-
+        const tiles = Array(this.rowSize).fill(1).map((_, index) => {
+            const value = TileRow.CHARS[this.state.valuesByTile[index]];
+            return (
+                <td
+                    className={"tile " + (value === "." ? 'hide-char' : '')}
+                    onClick={this.nextChar.bind(this, index)}>
+                    {value}
+                </td>
+            )
+        })
         return (
-            <td
-                className={"tile " + (currentLetter === "." ? 'hide-char' : '')}
-                onClick={this.nextChar}>
-                {currentLetter}
-            </td>
+            <tr>
+                { /* Clicking on the column header triggers the 'click' action on all tiles of the row */ }
+                <th className="tile" onClick={this.nextChar.bind(this, null)}>{this.header}</th>
+                {tiles}
+            </tr>
         );
     }
-
 }

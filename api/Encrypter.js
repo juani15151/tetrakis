4543c8c4dbe39df17dae820101ac3a23260f3fb7
juani15151 <15637690+juani15151@users.noreply.github.com>
2024-01-04 11:06:17 -0300

    Fix encrypter usage.

--- api/Encrypter.js
+++ api/Encrypter.js
@@ -4,18 +4,17 @@ const algorithm = "aes-128-cbc";
 const secretKey = crypto.randomBytes(16); // TODO: Read from environment variable.
 const initializationVector = crypto.randomBytes(16);
 
-const cipher = crypto.createCipheriv(algorithm, secretKey, initializationVector);
-const decipher = crypto.createDecipheriv(algorithm, secretKey, initializationVector);
-
 class Encrypter {
     static encrypt(text) {
-        cipher.write(text);
-        return cipher.end();
+        const cipher = crypto.createCipheriv(algorithm, secretKey, initializationVector);
+        cipher.update(text);
+        return cipher.final().toString('hex');
     }
 
     static decrypt(text) {
-        decipher.read(text)
-        return decipher.read();
+        const decipher = crypto.createDecipheriv(algorithm, secretKey, initializationVector);
+        decipher.update(text, 'hex')
+        return decipher.final().toString();
     }
 
     static getUUID() {
